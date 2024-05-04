7b70f83a021804b2b401e3aaed945cf543e2b5e9
sravantorra <sravantorra00@gmail.com>
2024-05-04 22:03:25 +0530

    Tectstack updated icons etc

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Layout from './Components/Layout/Layout';
 import About from './Pages/About/About';
+import Techstack from './Pages/Techstack/Techstack';
+
+
 function App() {
   return (
    <>
    <Layout/>
-   <div className="container"><About/></div>
+   <div className="container"><About/>
+   <Techstack/>
+   </div>
    </>
   );
 }
---
+++ client/src/Pages/Techstack/Techstack.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Techstack.css'
+import { TechstackList } from '../../utils/TechstackList'
+const Techstack = () => {
+    return (
+        <>
+            <div className="container techstack">
+                <h2 className='col-12 mt-3 ml-1 text-center'>Technologies Stack</h2><hr />
+                <p className='pb-3 text-center'>👉 including Programming Languages,frameworks,databases,front-end,
+                    back-end tools,and APIs</p>
+            <div className="row">
+                {TechstackList.map(tech=>(
+                    <div key={tech._id} className="col-md-3">
+                        <div className="card m-2">
+                            <div className="card content">
+                                <div className="card-body">
+                                    <div className="media d-flex justify-content-center">
+                                        <div className="alig-self-center">
+                                        <tech.icon className='tech-icon'/>
+                                        </div>
+                                        <div className="media-body">
+                                            <h5>{tech.name}</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Techstack
