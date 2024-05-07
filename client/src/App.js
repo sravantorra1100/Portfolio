3d2ade5f55c70cfbe7e6c9f79c489a8445ef293c
sravantorra <sravantorra00@gmail.com>
2024-05-07 13:59:25 +0530

    7/5/24 contact component updated and effects updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Layout from './Components/Layout/Layout';
 import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
 import Educations from './Pages/Educations/Educations';
 import Projects from './Pages/Projects/Projects';
 import Techstack from './Pages/Techstack/Techstack';
@@ -16,6 +17,12 @@ function App() {
    <Techstack/>
    <Projects/>
    <WorkExp/>
+   <Contact/>
+   </div>
+   <div className="footer mb-3 ms-3">
+    <h4 className='text-center'>
+      Made With 😍 Techinfoyt &copy; 2023
+    </h4>
    </div>
    </>
   );
